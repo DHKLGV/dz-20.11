8f4589dc132678a22553e50fa04807beb405f6c4
Inal <vipdahkilgov@gmail.com>
2022-11-25 21:20:09 +0300

    готово

---
+++ dz 25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Первое число: ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Второе число: ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = a1;
+
+for (int i = 1; i < a2; i++)
+{
+    sum = sum * a1;
+}
+Console.WriteLine($"{a1} в степени {a2} = " + sum);
---
+++ dz 29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int size = 8;
+
+int[] array = new int[8];
+
+Random a1 = new Random();
+
+Console.WriteLine("Массив");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = a1.Next(0, 8);
+
+    Console.Write("{0} ", array[i]);
+}
